afc5240d5932eb2597a6d3f5756028c15a8c3ee3
Shubham Sharma <112186685+Shubham13th@users.noreply.github.com>
2025-08-26 02:11:44 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,4 +1,19 @@
 function getFormvalue() {
     //Write your code here
 
+	 function getFormvalue() {
+            //Write your code here
+            event.preventDefault();
+            const form = document.getElementById('form1');
+
+            const formData = new FormData(form);
+
+            let output = "";
+            formData.forEach((value, key) => {
+                output += `${value} `;
+            });
+
+            alert(output.trim());
+        }
+
 }
